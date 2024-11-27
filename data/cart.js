8f46169cc52b092e097e2e8ebb812cbe1e225b9d
lilyhouse <houselily010@gmail.com>
2024-11-27 14:50:39 +0700

    fix price formating and radio-selector

--- data/cart.js
+++ data/cart.js
@@ -7,22 +7,22 @@ export const cart = [{
 }];
 
 export function addToCart(productId) {
-    const itemQuantity = Number(document.querySelector(`.js-product-quantity-${productId}`).value);
-  
-    let matchingItem;
-  
-    cart.forEach((cartItem) => {
-      if (cartItem.productId === productId) {
-        matchingItem = cartItem;
-      }
-    });
-  
-    if (matchingItem) {
-      matchingItem.quantity += itemQuantity;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: itemQuantity
-      });
+  const itemQuantity = Number(document.querySelector(`.js-product-quantity-${productId}`).value);
+
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId === productId) {
+      matchingItem = cartItem;
     }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += itemQuantity;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: itemQuantity
+    });
   }
+}
