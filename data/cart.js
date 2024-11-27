83c0c8df8a0dfd5cdf8a48e66de011d317ac59ba
lilyhouse <houselily010@gmail.com>
2024-11-27 15:17:24 +0700

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
 }, {
@@ -25,6 +25,7 @@ export function addToCart(productId) {
       quantity: itemQuantity
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -37,4 +38,9 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+  saveToStorage();
+}
+
+export function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
