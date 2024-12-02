5cc863286afb648864b9fa51ece149a55d37b871
lilyhouse <houselily010@gmail.com>
2024-12-02 15:41:50 +0700

    add jasmine testing framework

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,7 +1,6 @@
 import {formatCurrency} from '../scripts/utilis/money.js';
 
 console.log('test suite: formatCurrency');
-
 console.log('converts cents into dollars');
 if (formatCurrency(2095) === '20.95') {
     console.log('passed');
